11a3800254e594f8142011e6454a8441a592157d
Bruno Nunes <brunooliveiranunes60@gmail.com>
2023-11-17 02:37:59 -0300

    moongose adicionado

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const photosRoutes = require("./src/routes/photoRoutes");
 
 require("dotenv").config();
 
 const app = express();
+mongoose
+  .connect(process.env.DBKEY)
+  .then(() => {
+    app.emit("ready");
+  })
+  .catch((e) => console.log(e));
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
@@ -19,7 +27,8 @@ const port = process.env.PORT;
 app.get("/", (req, res) => {
   res.status(200).json({ message: "API online 😎" });
 });
-
-app.listen(port, () => {
-  console.log(`API rodando em http://localhost:${port}`);
+app.on("ready", () => {
+  app.listen(port, () => {
+    console.log(`API rodando em http://localhost:${port}`);
+  });
 });
--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -1,8 +1,22 @@
 const fs = require("fs");
 const Photos = require("../models/PhotoModel")
 
-exports.create = (req, res) => {
-  res.status(200).json({ message: "rota create" });
+exports.create = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { file } = req;
+
+    const photo = new Photos({
+      name,
+      src: file.path,
+    });
+
+    await photo.save();
+
+    res.status(200).json({ photo, message: "Imagen salva com sucesso." });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao salvar imagem." });
+  }
 };
 exports.getAll = (req, res) => {
   res.status(200).json({ message: "rota getall" });
